fe8f3f2c668f44b69842bcdb7c28ab91a79fcf31
tonym <tony.marques@live.fr>
2024-09-27 17:17:20 +0200

    Add category to article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -23,8 +23,18 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 class ArticleController extends AbstractController
 {
     #[Route(path: '/articles', name: 'articles')]
-    public function index(ArticleRepository $articleRepository, SerializerInterface $serializer): JsonResponse
+    public function index(ArticleRepository $articleRepository, SerializerInterface $serializer, Request $request, CategoryRepository $categoryRepository): JsonResponse
     {
+        $categoryName = $request->query->get('category');
+
+        $category = $categoryRepository->findOneBy(["slug" => $categoryName]);
+
+        if($category){
+            $articlesJson = $serializer->serialize($category->getArticles(), "json", ['groups' => ['article:read']]);
+    
+            return new JsonResponse($articlesJson, Response::HTTP_OK, [], true);
+        }
+
         $articles = $articleRepository->findBy([], ["createdAt" => "DESC"]);
 
         $articlesJson = $serializer->serialize($articles, "json", ['groups' => ['article:read']]);
@@ -35,15 +45,10 @@ class ArticleController extends AbstractController
     #[Route(path: "/articles/create", name: "article_create")]
     public function create(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, ValidatorInterface $validator, SluggerInterface $slugger, CategoryRepository $categoryRepository, LoggerInterface $logger)
     {
-        // $article = $serializer->deserialize($request->getContent(), Article::class, "json");
 
-        // $articleArray = $request->toArray();
-        // dd("toto");
-        // dd($request->files->has('mainImage1'));
 
         $data = $request->request->all();
-        // $data = $request->toArray();
-        $logger->info('Données reçues : ' . json_encode($data)); // Enregistre les
+    
 
         $article = new Article();
         $article->setTitle($data['title'] ?? '');
@@ -54,7 +59,6 @@ class ArticleController extends AbstractController
         $category = $categoryRepository->findOneBy(["name" => $data["category"]]);
 
         $article->addCategory($category);
-
 
         if ($request->files->has('mainImage1')) {
             $mainImage1File = $request->files->get('mainImage1');
@@ -70,7 +74,6 @@ class ArticleController extends AbstractController
                     );
                     $article->setMainImage1($newFilename1);
                 } catch (FileException $e) {
-                    // Gérer l'exception
                     return new JsonResponse(['error' => 'Une erreur est survenue lors du téléchargement de l\'image 1'], Response::HTTP_INTERNAL_SERVER_ERROR);
                 }
             }
