8b6a15cef6062ff9ba9ac30ce987495f59f8e318
tonym <tony.marques@live.fr>
2024-09-27 11:34:59 +0200

    CategoryController > Create index method

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,8 +16,18 @@ use Symfony\Component\Serializer\SerializerInterface;
 #[Route(path: "/api", name: "api_")]
 class CategoryController extends AbstractController
 {
+    #[Route(path: '/categories', name: 'categories')]
+    public function index(CategoryRepository $categoryRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $categories = $categoryRepository->findBy([], ["createdAt" => "DESC"]);
+
+        $categoriesJson = $serializer->serialize($categories, "json", ['groups' => ['category:read']]);
+
+        return new JsonResponse($categoriesJson, Response::HTTP_OK, [], true);
+    }
+
     #[Route('/categories/create', name: 'category_create')]
-    public function index(Request $request, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
+    public function create(Request $request, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
     {
         $category = $serializer->deserialize($request->getContent(), Category::class, "json");
 
@@ -25,7 +36,7 @@ class CategoryController extends AbstractController
         $em->persist($category);
         $em->flush();
 
-        $categoryJson = $serializer->serialize($category, "json");
+        $categoryJson = $serializer->serialize($category, "json",  ['groups' => ['category:read']]);
 
         return new JsonResponse($categoryJson, Response::HTTP_CREATED,  [], true);
     }
